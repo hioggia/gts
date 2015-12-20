ebf458f4f435534cbf90462f14b17d9b3e93b905
hioggia <mikokuu@gmail.com>
2015-12-21 00:28:44 +0800

    update

--- kajikano/casino_slot.js
+++ kajikano/casino_slot.js
@@ -1,2 +1,6 @@
-function tp(s){if($('#pop-captcha').children().size()==0){$(s).trigger('tap')}};function tz(s){var _=$('div',s),__=_.size()-1,___=0;_.each(function(i,____){___+=~~____.className.split('_')[1]*Math.pow(10,__-i)});return ___}function m(p,g){if(tz('.prt-medal')<=p || tz('.prt-medal')>=g){console.info('end');return}if(tz('.prt-bet')==0){o=0;var r=tz('.prt-won');l.push(r);console.info(r);tp('.prt-bet-max')}else if(o++>30){location.reload()}setTimeout(m,1000*2)}Object.defineProperty(window,'v',{get:function(){var t=0;l.forEach(function(v){t+=v;});return t-l.length*b}});var l=[],o=0,b=300;
-setTimeout(function(){m(1000000,Infinity)},1000*5);
+(function(){function tp(s){if($('#pop-captcha').children().size()==0){$(s).trigger('tap')}};
+function tz(s){var _=$('div',s),__=_.size()-1,___=0;_.each(function(i,____){___+=~~____.className.split('_')[1]*Math.pow(10,__-i)});return ___}
+function m(p,g){if(tz('.prt-medal')<=p || tz('.prt-medal')>=g){console.info('end');return}if(tz('.prt-bet')==0){ela.html('累计：'+l.length+'次，成绩：'+v);o=0;var r=tz('.prt-won');l.push(r);tp('.prt-bet-max');}else if(o++>30){location.reload()}setTimeout(m,1000*2)}
+Object.defineProperty(window,'v',{get:function(){var t=0;l.forEach(function(v){t+=v;});return t-l.length*b}});var l=[],o=0,b=300,ela;
+function g(){ela=$('<div></div>').appendTo('.prt-controller').css({position:'absolute',top:'100%',left:'0',color:'white'});;createjs.Ticker.setFPS(300);m(1000000,Infinity)}
+setTimeout(g,1000*5);})();
